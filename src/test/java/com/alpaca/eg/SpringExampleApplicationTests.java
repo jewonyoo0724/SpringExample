8a0e61942e9bd22acec7425b70c8d1c24959da3d
Jewon Yoo <skywalker0724@gmail.com>
2023-07-04 21:34:20 +0900

    - spring start

---
+++ src/test/java/com/alpaca/eg/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpaca.eg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
